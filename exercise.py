49d4d21f0c21ef9179a7f882afb93206f9aa5d33
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-11-06 21:19:05 +0800

    added in try catch for first question

--- exercise.py
+++ exercise.py
@@ -3,20 +3,31 @@ Personal practice on python
 """
 import random, string
 
+
 def question_one(strs):
     """
     Q1) Write a program to sort the characters of a string such that the it follows following format:
     uppercase letters, lowercase letters then numbers. Punctuation is not required.
     """
-    print("Question 1 Answers:")
-    letters = [s for s in strs if s.isalpha()]
-    sorted_letters = "".join(sorted(letters))
-    numbers = [s for s in strs if not s.isalpha()]
-    sorted_numbers = "".join(sorted(numbers))
+    try:
+        letters = [s for s in strs if s.isalpha()]
+        sorted_letters = "".join(sorted(letters))
+        numbers = [s for s in strs if not s.isalpha()]
+        sorted_numbers = "".join(sorted(numbers))
 
-    print(f"Input: {strs}")
-    print(f"Output: {sorted_letters}{sorted_numbers}")
-    print("")
+        print("Question 1 Answers:")
+        print(f"Input: {strs}")
+        print(f"Output: {sorted_letters}{sorted_numbers}")
+        print("")
+    except TypeError as err:
+        print(err, "; Skipping to the next question.")
+
+
+def question_two():
+    """
+    Write a function to count the number of strings from the given list of strings where the strings
+    must have a length more than or equal to 2 and its first and last character must match.
+    """
 
 
 if __name__ == "__main__":
