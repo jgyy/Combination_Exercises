62a8c447bd29a5bacdb7a421eae95548d97f18c7
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-11-06 22:08:54 +0800

    completed question 3, time for next question

--- exercise.py
+++ exercise.py
@@ -20,13 +20,14 @@ def question_one(strs):
         print(f"Output: {sorted_letters}{sorted_numbers}")
         print("")
     except TypeError as err:
-        print(err, "; Skipping to the next question.")
+        print(err, "; Skipping to question 2.")
 
 
 def question_two(strs):
     """
-    Write a function to count the number of strings from the given list of strings where the strings
-    must have a length more than or equal to 2 and its first and last character must match.
+    Write a function to count the number of strings from the given list of strings
+    where the strings must have a length more than or equal to 2 and its first and
+    last character must match.
     """
     try:
         letters = [s for s in strs if s[0] == s[-1] and len(s) >= 2]
@@ -40,13 +41,36 @@ def question_two(strs):
     except TypeError as err:
         print(err, "; Skipping to the next question.")
 
+
+def question_three(strs):
+    """
+    Write a function that accepts a string, calculates the number of upper case
+    letters and lower case letters and returns the values as a dictionary. The
+    program (not the function) is to print the expected output defined below.
+    """
+    try:
+        letter_lower = [s for s in strs if s.islower()]
+        lower_count = str(len(letter_lower))
+        letter_upper = [s for s in strs if s.isupper()]
+        upper_count = str(len(letter_upper))
+
+        print("Question 3 Answers:")
+        print(f"Original String: {strs}")
+        print(f"No. of Upper case characters: {lower_count}")
+        print(f"No. of Lower case characters: {upper_count}")
+        print("")
+    except TypeError as err:
+        print(err, "; Skipping to the next question.")
+
+
 if __name__ == "__main__":
     length_1 = random.randint(1, 99)
     letter_1 = string.ascii_lowercase + string.ascii_uppercase + string.digits
     result_1 = ''.join(random.choice(letter_1) for _ in range(length_1))
     question_one(result_1)
 
-    length_2 = random.randint(1, 9)
+    length_2 = random.randint(1, 16)
     letter_2 = "ab"
     result_2 = [''.join(random.choice(letter_2) for _ in range(length_2)) for _ in range(length_2)]
     question_two(result_2)
+    question_three(result_1)
